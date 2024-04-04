c43d20d09941ca27d8e3973b22c2b41483803f97
Stanley Hughes <codemusic@mail.ru>
2024-04-04 21:35:29 +0300

    slick slider - remove mobileFirst

--- js/slick/slider.js
+++ js/slick/slider.js
@@ -5,7 +5,6 @@ $(document).ready(function(){
         slidesToScroll: 1,
         prevArrow: '<button class="slick-prev slick-arrow" type="button"></button>',
         nextArrow: '<button class="slick-next slick-arrow" type="button"></button>',
-        mobileFirst: true,
         responsive: [
             {
                 breakpoint: 1224,
