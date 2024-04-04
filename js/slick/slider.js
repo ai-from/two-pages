c84d83dcbf29cfd4bee555125cfaf8eaa3310c37
Stanley Hughes <codemusic@mail.ru>
2024-04-04 22:23:40 +0300

    add breakpoints

--- js/slick/slider.js
+++ js/slick/slider.js
@@ -5,25 +5,25 @@ $(document).ready(function(){
         slidesToScroll: 1,
         prevArrow: '<button class="slick-prev slick-arrow" type="button"></button>',
         nextArrow: '<button class="slick-next slick-arrow" type="button"></button>',
-        // responsive: [
-        //     {
-        //         breakpoint: 1224,
-        //         settings: {
-        //           slidesToShow: 3
-        //         }
-        //     },
-        //     {
-        //         breakpoint: 992,
-        //         settings: {
-        //           slidesToShow: 2
-        //         }
-        //     },
-        //     {
-        //         breakpoint: 768,
-        //         settings: {
-        //           slidesToShow: 1
-        //         }
-        //     }
-        // ]
+        responsive: [
+            {
+                breakpoint: 1224,
+                settings: {
+                  slidesToShow: 3
+                }
+            },
+            {
+                breakpoint: 992,
+                settings: {
+                  slidesToShow: 2
+                }
+            },
+            {
+                breakpoint: 768,
+                settings: {
+                  slidesToShow: 1
+                }
+            }
+        ]
     });
 });
