57efa72cfe8689cf58d21c80b8dc56b21e5d448e
Stanley Hughes <codemusic@mail.ru>
2024-04-04 21:27:07 +0300

    slick slider - settings

--- js/slick/slider.js
+++ js/slick/slider.js
@@ -10,19 +10,22 @@ $(document).ready(function(){
             {
                 breakpoint: 1224,
                 settings: {
-                  slidesToShow: 3
+                  slidesToShow: 3,
+                  slidesToScroll: 1
                 }
             },
             {
                 breakpoint: 992,
                 settings: {
-                  slidesToShow: 2
+                  slidesToShow: 2,
+                  slidesToScroll: 1
                 }
             },
             {
                 breakpoint: 768,
                 settings: {
-                  slidesToShow: 1
+                  slidesToShow: 1,
+                  slidesToScroll: 1
                 }
             }
         ]
